fff411b05a13abff2cb500482dd44c61ca43f763
GADO89 <gadomahmoud377@gmail.com>
2024-04-05 21:58:14 +0200

    handle submit of update and save

--- src/app/components/options/options.component.ts
+++ src/app/components/options/options.component.ts
@@ -1,10 +1,11 @@
 
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {Student} from "../../model/student";
 import {StudentService} from "../../services/Student.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {response} from "express";
+import {Spacevalidator} from "../../model/spacevalidator";
 
 @Component({
   selector: 'app-options',
@@ -40,10 +41,10 @@ export class OptionsComponent implements OnInit {
 
     this.studentGroup = this.formBuilder.group({
       student: this.formBuilder.group({
-        userName:[''],
-        age:[''],
-        address:[''],
-        phone:[''],
+        userName:new FormControl('',[Validators.required, Validators.minLength(5),Spacevalidator.noOnlyWithSpace]),
+        age:new FormControl('',[Validators.required, Validators.maxLength(2),Validators.pattern("^[0-9]*$"),Spacevalidator.noOnlyWithSpace]),
+        address:new FormControl('',[Validators.required]),
+        phone:new FormControl('',[Validators.required, Validators.maxLength(11), Validators.minLength(11),Validators.pattern("^[0-9]*$"),Spacevalidator.noOnlyWithSpace]),
         gender:['MALE'],
 
       })
@@ -61,8 +62,24 @@ export class OptionsComponent implements OnInit {
   }   getGender(){
     return this.studentGroup.get("student")?.value.gender;
   }
+  get userName(){
+    return this.studentGroup.get("student.userName");
+  }
+  get age(){
+    return this.studentGroup.get("student.age");
+  }   get address(){
+    return this.studentGroup.get("student.address");
+  }   get phone(){
+    return this.studentGroup.get("student.phone");
+  }   get gender(){
+    return this.studentGroup.get("student.gender");
+  }
+
   Done() {
     const stu=new Student(this.id,this.getUserName(), this.getGender(), this.getAge(), this.getPhone(), this.getAddress());
+    if(this.studentGroup.invalid){
+      this.studentGroup.markAllAsTouched();
+    } else {
       if (this.id ==0){
         this.serviceStudent.addStudent(stu).subscribe(
           response => {
@@ -80,6 +97,7 @@ export class OptionsComponent implements OnInit {
           }
         )
       }
+    }
   }
   showMessage(){
     setTimeout(() => {
